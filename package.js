b9b3c2ece9447c5b05330f72e3876e07a2b2a295
ajduke <ajduke@about.me>
2014-03-17 16:40:03 +0530

    Initial Commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    summary: "jQuery plugin providing a Twitter Bootstrap user interface for managing tags, repackaged for Meteor"
+});
+
+Package.on_use(function (api) {
+	// dependecies required by package
+    api.use('jquery', 'client'); 
+    api.use('bootstrap-3', 'client');
+    
+    // adding the required file for package
+    api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', 'client');
+    api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', 'client');
+	api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput-angular.js', 'client');
+    
+});
