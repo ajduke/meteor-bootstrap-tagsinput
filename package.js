22e42b1757346b5ee474c851d119e403c6ca3a45
apendua <apendua@gmail.com>
2014-03-27 10:34:27 +0100

    Cleaning up

--- package.js
+++ package.js
@@ -3,13 +3,12 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-	// dependecies required by package
+    // dependecies required by package
     api.use('jquery', 'client'); 
     api.use('bootstrap-3', 'client');
     
     // adding the required file for package
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', 'client');
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', 'client');
-	api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput-angular.js', 'client');
-    
+    api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput-angular.js', 'client');
 });
