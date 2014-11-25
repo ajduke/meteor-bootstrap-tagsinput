d8210ff952f9d321d4bd9fa3e237af9cb4b55f07
ajduke <ajduke@about.me>
2014-11-25 22:59:05 +0530

    default version of package 0.6.x which will have the bootstrap dependecy as default

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'ajduke:bootstrap-tagsinput',
     summary: 'jQuery plugin providing a Twitter Bootstrap user interface for managing tags, repackaged for Meteor',
-    version: '0.5.0',
+    version: '0.6.0',
     git: "https://github.com/ajduke/meteor-bootstrap-tagsinput.git"
 });
 
 Package.on_use(function (api) {
     // dependecies required by package
-    api.use('jquery@1.0.0', 'client');
-
+    api.use('jquery@1.0.1', 'client');
+    api.use('mizzao:bootstrap-3@3.3.1','client')
     // adding the required file for package
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', 'client');
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', 'client');
